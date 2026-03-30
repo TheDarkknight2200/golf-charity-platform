1c5045e7197967f37469b67376ed51609ccc44ea
TheDarkknight2200 <laminetine2200@gmail.com>
2026-03-30 20:51:00 +0000

    test: disable middleware temporarily

--- middleware.js
+++ middleware.js
@@ -1,56 +1,7 @@
-import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  let res = NextResponse.next({
-    request: { headers: req.headers },
-  })
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get(name) {
-          return req.cookies.get(name)?.value
-        },
-        set(name, value, options) {
-          req.cookies.set({ name, value, ...options })
-          res = NextResponse.next({
-            request: { headers: req.headers },
-          })
-          res.cookies.set({ name, value, ...options })
-        },
-        remove(name, options) {
-          req.cookies.set({ name, value: '', ...options })
-          res = NextResponse.next({
-            request: { headers: req.headers },
-          })
-          res.cookies.set({ name, value: '', ...options })
-        },
-      },
-    }
-  )
-
-  const { data: { session } } = await supabase.auth.getSession()
-
-  if (!session) {
-    return NextResponse.redirect(new URL('/auth', req.url))
-  }
-
-  if (req.nextUrl.pathname.startsWith('/admin')) {
-    const { data: profile } = await supabase
-      .from('profiles')
-      .select('is_admin')
-      .eq('id', session.user.id)
-      .single()
-
-    if (!profile?.is_admin) {
-      return NextResponse.redirect(new URL('/dashboard', req.url))
-    }
-  }
-
-  return res
+  return NextResponse.next()
 }
 
 export const config = {
